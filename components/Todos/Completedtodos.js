dedf29bf2ae70fd3e924e213332bc2fa86782970
akshaysable02 <akshaysable097@gmail.com>
2024-01-19 20:37:54 +0530

    Reflecting on the accomplishments

--- components/Todos/Completedtodos.js
+++ components/Todos/Completedtodos.js
@@ -1,25 +1,46 @@
 import React from "react";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button,Row,Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Completedtodos() {
   return (
     <div
-      className="d-flex justify-content-center align-items-center"
+    className="d-flex justify-content-center align-item-center"
       style={{ height: "30vh" }}
     >
       <Card
-        style={{
-          width: "800px",
-          padding: "30px",
-          border: "2px solid grey ",
-        }}
+        style={{ width: "1000px", padding: "30px", border: "2px solid black" }}
       >
-        <Card.Title>Completed Task</Card.Title>
-        <Card.Body>
-
-            <p>Revise React</p>
-
+       <Card.Title>Todo's List</Card.Title>
+        <Card.Body
+          style={{
+            fontSize: "20px",
+            display: "flex",
+            justifyContent: "space-between",
+          }}
+        >
+          <Row>
+            {props.completedtodos.map((todo) => (
+              <Col key={todo.id}>
+                <div className="d-flex align-items-center">
+                  <p style={{ marginRight: "10px" }}>{todo.task}</p>
+                  <Button
+                    variant="success"
+                    style={{ marginRight: "5px" }}
+                    onClick={() => completedHandler(todo.id)}
+                  >
+                    Completed
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => deleteHandler(todo.id)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </Col>
+            ))}
+          </Row>
         </Card.Body>
       </Card>
     </div>
